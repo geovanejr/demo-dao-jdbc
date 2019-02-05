5666cc8e6263a406619d3de7f03fa64122d25ebd
Geovane Junior <geovane.gjunior@gmail.com>
2019-02-04 23:28:57 -0200

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,8 @@ public class Program {
 	public static void main(String[] args) {
 
 		Department dep = new Department(1, "Books");
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 2400.0, dep);
 		
